c2a2128ee94e84021aa21c0aaac897236323edbc
Lanny Rivest <lanny.rivest@gmail.com>
2020-09-16 18:53:01 -0400

    Sanitizes user inputs

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,18 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+const value = [`${args[0]}`];
 
-pool.query(`
-SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+const queryString = `SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students on students.id = student_id
 JOIN cohorts on cohorts.id = cohort_id
-WHERE cohorts.name = '${args[0]}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`)
+`;
+
+pool.query(queryString, value)
   .then(res => {
     res.rows.forEach(teachers => {
       const { teacher, cohort } = teachers;
